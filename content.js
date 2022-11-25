908f33e873ce93528afc592bf7480b07e1b0b105
Netah <channelservice12@gmail.com>
2022-11-25 22:03:37 +0200

    Added feature to select different rating platforms

--- content.js
+++ content.js
@@ -9,24 +9,45 @@ const runTester = () => {
         const htmlValue = document.getElementsByClassName('previewModal--player-titleTreatment-logo')[0].alt;
         const fixedValueForRotten = htmlValue.split(' ').join('_').toLowerCase();
         chrome.storage.sync.get("openWithRootenButtonColor", function (value) {
-            let colorOfBtn = "orange";
-            console.log("read value:", value);
-            if(value.openWithRootenButtonColor && String(value.openWithRootenButtonColor).startsWith("#")) {
-                console.log("IT IS");
-                colorOfBtn = value.openWithRootenButtonColor;
-            };
-            console.log(htmlValue, fixedValueForRotten);
-            console.log(colorOfBtn);
-            const buttonsInnerHtml = document.getElementsByClassName('buttonControls--container')[0].innerHTML;
-            document.getElementsByClassName('buttonControls--container')[0].innerHTML = `
-            <button class="color-primary hasLabel hasIcon ltr-1jtux27 openOnRottenBtn" style="background-color: ${colorOfBtn} !important;" tabindex="-1" onclick="window.open('https://www.rottentomatoes.com/tv/${fixedValueForRotten}','popUpWindow','height=500,width=400,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');" type="button">
-            <span class="ltr-j0gpa2">Open with Rotten!</span>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Rotten_Tomatoes.svg/1009px-Rotten_Tomatoes.svg.png" style="
-                width: 1em;
-                margin-left: 5px;
-            ">
-            </button>
-            ${buttonsInnerHtml}`;
+
+            chrome.storage.sync.get("platformToOpenWith", function (platformValue) {
+                let platform = 0;
+                if(platformValue.platformToOpenWith) {
+                    if(platformValue.platformToOpenWith == 'rotten') {
+                        platform = 0;
+                    } else if(platformValue.platformToOpenWith == 'letterboxd') {
+                        platform = 1;
+                    }
+                }
+                let colorOfBtn = "orange";
+                console.log("read value:", value);
+                if(value.openWithRootenButtonColor && String(value.openWithRootenButtonColor).startsWith("#")) {
+                    console.log("IT IS");
+                    colorOfBtn = value.openWithRootenButtonColor;
+                };
+
+                console.log(htmlValue, fixedValueForRotten);
+                console.log(colorOfBtn);
+
+                const buttonsInnerHtml = document.getElementsByClassName('buttonControls--container')[0].innerHTML;
+                document.getElementsByClassName('buttonControls--container')[0].innerHTML = `
+                ${platform === 1 ? `
+                <button class="color-primary hasLabel hasIcon ltr-1jtux27 openOnRottenBtn" style="background-color: blue !important;" tabindex="-1" onclick="window.open('https://letterboxd.com/film/${fixedValueForRotten}','popUpWindow','height=500,width=400,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');" type="button">
+                    <span class="ltr-j0gpa2">Open with LetterBoxd!</span>
+                </button>
+                ` : `
+                <button class="color-primary hasLabel hasIcon ltr-1jtux27 openOnRottenBtn" style="background-color: ${colorOfBtn} !important;" tabindex="-1" onclick="window.open('https://www.rottentomatoes.com/tv/${fixedValueForRotten}','popUpWindow','height=500,width=400,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');" type="button">
+                    <span class="ltr-j0gpa2">Open with Rotten!</span>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Rotten_Tomatoes.svg/1009px-Rotten_Tomatoes.svg.png" style="
+                        width: 1em;
+                        margin-left: 5px;
+                    ">
+                </button>
+                ` }
+                ${buttonsInnerHtml}`;
+
+
+            });
         } );
     }
 };
--- popup/script.js
+++ popup/script.js
@@ -1,9 +1,20 @@
 const colorPicker = document.getElementById('colorPicker');
+const platformPicker = document.getElementById('platformPicker');
+
 colorPicker.addEventListener('change', () => {
     const updatedColor = document.getElementById('colorPicker');
     console.log(updatedColor.value)
     chrome.storage.sync.set({openWithRootenButtonColor: updatedColor.value}, function () {
-        console.log("had saved!");
+        console.log("color had saved!");
+        // chrome.storage.sync.get("test", function (value) { console.log("read value: ", value.test); } );
+    });
+});
+
+platformPicker.addEventListener('change', () => {
+    const updatedPlatform = document.getElementById('platformPicker');
+    console.log(updatedPlatform.value)
+    chrome.storage.sync.set({platformToOpenWith: updatedPlatform.value}, function () {
+        console.log("platform had saved!");
         // chrome.storage.sync.get("test", function (value) { console.log("read value: ", value.test); } );
     });
 });
